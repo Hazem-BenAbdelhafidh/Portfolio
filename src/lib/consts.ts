4a2f7c69149d120134deaa3e64703fb1483fa6e3
Hazem-BenAbdelhafidh <ben_abdelhafidh_hazem@yahoo.com>
2025-01-13 03:38:36 +0100

    update work activities to include client meeting contributions and adjust API migration performance metrics

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -6,11 +6,12 @@ export const work = [
     activities: [
       "Refined React projects by integrating new technologies like React Query, Zustand, Tailwind and Zod",
       "Maintained and enhanced existing projects using frameworks like Vue and React, effectively integrated new features and fixed bugs to improve functionality",
+      "Participated in client meetings as a technical support specialist and team lead to resolve bugs in real time and contribute to project decision-making.",
       "Designed and developed an advanced webinar application utilizing Go, MySQL, React and WebSockets, engineered to support a capacity of 20,000 concurrent users with interactive chat and question submission functionalities",
       "Developed and deployed a npm package used by 4 different clients for custom requirements",
       "Constructed and maintained a SPFx webpart used by users from more than 10 different countries for a specific client, enhancing its integration within their intranet environment",
       "Prepared technical specifications and provided clear development guidelines in a 2-person team and conceived a user-friendly dashboard for custom form creation and management",
-      "Migrated legacy API endpoints from PHP to Go, resulting in API response times that are up to 100% faster",
+      "Migrated legacy API endpoints from PHP to Go, resulting in API response times that are up to 80% faster",
       "Wrote scripts in both Go and Typescript to migrate new clients data into our databases",
     ],
   },
